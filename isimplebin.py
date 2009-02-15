b13a4527eb34a1d0a2735205aaa2d4473d73b16a
Ghais Issa <ghais@Inferno.(none)>
2009-02-15 16:35:01 -0500

    a bit of refactoring created a simple admin page

--- isimplebin.py
+++ isimplebin.py
@@ -3,7 +3,6 @@ import logging
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from datetime import datetime
 from datetime import timedelta
@@ -13,13 +12,7 @@ from pygments import highlight
 from pygments.lexers import PythonLexer
 from formater import CodeHtmlFormatter
 
-class PostModel(db.Model):
-    author = db.UserProperty()
-    content = db.TextProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
-    modified = db.BooleanProperty()
-    modifier = db.UserProperty()
-    expiry = db.DateTimeProperty()
+from date_model import PostModel
 
 
 class MainPage(webapp.RequestHandler):
@@ -49,7 +42,17 @@ class Form(webapp.RequestHandler):
         if users.get_current_user():
             post_model.author = users.get_current_user()
 
-        post_model.content = self.request.get('content')
+            
+        post_expiry = self.request.get('expiry')
+        
+        if post_expiry == 'd':
+            post_model.expiry = datetime.now()  + timedelta(days=1)
+        elif post_expiry == 'm':
+            post_model.expiry = datetime.now()  + timedelta(days=1)
+        elif post_expiry == 'f':
+            post_model.expiry = None
+        
+        post_model.content = self.request.get('content')        
         post_model.modified = False
         post_model.modifier = None
         post_model.save()
@@ -59,7 +62,7 @@ class Update(webapp.RequestHandler):
     def post(self):
         db_id = self.request.uri.split("/")[-1]
         view = PostModel.get_by_id(int(self.request.uri.split("/")[-1]))
-        content = self.request.get('content')        
+        content = self.request.get('content')
         view.content = content
         view.modified = True
         if users.get_current_user():
@@ -108,6 +111,7 @@ class Post(webapp.RequestHandler):
             'line': line.replace("@@", ""),
             'id' : self.request.uri.split("/")[-1],
             'view' : view,
+            'expiry': view.expiry,
         }
         if view :
             path = os.path.join(os.path.dirname(__file__), 'html/view.html')
